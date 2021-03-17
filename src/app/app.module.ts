95e61bb577d19d7b0c192da47339f22173f4719e
Joffy Jose <joffy.jose.jfy@gmail.com>
2021-03-17 13:08:07 +0530

    login routing to next page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,57 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  accountDetails = {
+  accountDetails: any = {
     1000: { accno: 1000, name: "userone", balance: 6000, password: "user1" },
     1001: { accno: 1001, name: "usertwo", balance: 9000, password: "user2" },
     1002: { accno: 1002, name: "userthree", balance: 7000, password: "user3" },
     1003: { accno: 1003, name: "userfour", balance: 8000, password: "user4" },
     1004: { accno: 1004, name: "userfive", balance: 61000, password: "user5" },
-}
- usrname="";
- pswd="";
- aim="poda mwoana"
- uname="enter use name"
-  constructor() { }
+  }
+  accno= "enter acco";
+  pswd = "";
+  aim = "pewer"
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-  getUsername(event:any)
-  {
-    this.usrname=event.target.value;
-    console.log(this.usrname);
-    
-  }
-  getPswd(event:any){
-    this.pswd=event.target.value;
-    console.log(this.pswd);
-    
-  }
-
-  login()
-  {
-    
-    
-  //     let accno = document.querySelector("#acno").value;
-  //     let pwd = document.querySelector("#pswd").value;
-  //     let data = Bank.getAccountdetails()
-  //     {
-  //         if (accno in data) {
-  //             // alert("exist")
-  //             if (pwd == data[accno]["password"]) {
-  //                 // alert ("login success")
-  //                 window.location.href = "banktbtn.htm";
-  //             }
-  //             else {
-  //                 alert("not success")
-  //             }
-  //         }
-  //         else {
-  //             alert("not an acc")
-  //         }
-  //     }
+  // getAccno(event: any) {
+  //   this.accnoo = event.target.value;
 
   // }
+  // getPswd(event: any) {
+  //   this.pswd = event.target.value;
+
+  // }
+
+  login() {
+    var accNuo = this.accno;
+    var pwd = this.pswd;
+    var data = this.accountDetails;
+
+    if (accNuo in data) {
+      var psw1 = data[accNuo].password;
+      if (pwd == psw1) {
+        console.log("login success");
+        this.router.navigateByUrl("dashboard")
+
+      }
+      else {
+        console.log("inncorrect pswd");
+
+      }
+    }
+    else {
+      console.log("no user account");
+
+    }
 
   }
 
