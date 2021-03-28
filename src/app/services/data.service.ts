d33159c1859e82cce6f412df08fa3f2b6c54eda8
jfyjse <joffy.jose.jfy@gmail.com>
2021-03-28 23:23:43 +0530

    bank app final, local storage implemented

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -6,45 +7,69 @@ import { Injectable } from '@angular/core';
 export class DataService {
   accountDetails: any = {
     1000: { accno: 1000, name: "kanappi", balance: 6000, password: "user1" },
-    1001: { accno: 1001, name: "jinjin", balance: 9000, password: "user2" },
-    1002: { accno: 1002, name: "himajal", balance: 7000, password: "user3" },
-    1003: { accno: 1003, name: "oiuhinj", balance: 8000, password: "user4" },
-    1004: { accno: 1004, name: "fiverr", balance: 61000, password: "user5" },
+    1001: { accno: 1001, name: "dybala", balance: 9000, password: "user2" },
+    1002: { accno: 1002, name: "kuttappan", balance: 7000, password: "user3" },
+    1003: { accno: 1003, name: "kuttappi", balance: 8000, password: "user4" },
+    1004: { accno: 1004, name: "jinjin", balance: 61000, password: "user5" },
   }
   currentUser:any;
+  currentBal:any;
 
   constructor() {
-    // this.getDetails();
+    this.getDetails();
    }
+   
+   saveDetails() {
+    localStorage.setItem("accountDetails", JSON.stringify(this.accountDetails))
+    if (this.currentUser) {
+      localStorage.setItem("currentUser", JSON.stringify(this.currentUser))
+    }
+    if (this.currentBal) {
+      localStorage.setItem("currentBal", JSON.stringify(this.currentBal))
+    }
+  }
+  
+  getDetails() {
+    if (localStorage.getItem("accountDetails")) {
+      this.accountDetails = JSON.parse(localStorage.getItem("accountDetails")|| '')
+    }
+    if (localStorage.getItem("currentUser")) {
+      {
+        this.currentUser = JSON.parse(localStorage.getItem("currentUser")||'')
+      }
 
-  // saveDetails()
-  // {
-  //   localStorage.setItem("accountDetails",JSON.stringify(this.accountDetails))
-  //   localStorage.setItem("currentUser",JSON.stringify(this.currentUser))
-  // }
+    }
 
-  // getDetails()
-  // {
-  //   this.accountDetails= localStorage.getItem("accountDetails")
-  //   this.currentUser= localStorage.getItem("currentUser")
-  // }
+    if (localStorage.getItem("currentBal")) {
+      {
+        this.currentBal = JSON.parse(localStorage.getItem("currentBal")||'')
+      }
+
+    }
+
+  }
+
   register(accno: any, password: any) {
     if (accno in this.accountDetails) {
       console.log("user exist log in");
       alert("please login")
+     
       return false;
 
     }
 
     this.accountDetails[accno] = {
       accno,
-      name: "new",
+      name: "new user",
       balance: 0,
       password
     }
-    // this.saveDetails()
+
+    
     alert("reg success")
     console.log(this.accountDetails);
+    this.saveDetails()
+    
     return true;
 
   }
@@ -60,7 +85,9 @@ export class DataService {
         console.log("login success");
         var userl= data[accno].name;
         this.currentUser=userl;
-        // this.saveDetails();
+        var ballm=data[accno].balance;
+        this.currentBal=ballm;
+        this.saveDetails();
         
         return true;
 
@@ -87,9 +114,12 @@ deposit(accno:any,pwd:any,amt:any)
     if (accno in data) {
       var psw1 = data[accno].password;
       if (pwd == psw1) {
-        // this.saveDetails();
+       
         console.log("login success");
         data[accno].balance+=ammt;
+        this.saveDetails()
+        
+        
         alert("acc credited wit :"+amt+" new bal= " + data[accno].balance)        
         
 
@@ -115,7 +145,7 @@ withdraw(accno:any,pwd:any,amt:any){
     if (accno in data) {
       var psw1 = data[accno].password;
       if (pwd == psw1) {
-        // this.saveDetails();
+  
         console.log("login success");
         if((data[accno].balance) < ammt)
         {
@@ -124,6 +154,7 @@ withdraw(accno:any,pwd:any,amt:any){
         else
         {
           data[accno].balance-=ammt;
+          this.saveDetails()
           alert("acc debited wit :"+amt+" new bal= " + data[accno].balance);
         }
         }
